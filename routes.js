04793c7e135f1cdf5b40867a1c74de91c93b6e95
cgdouthitt <46352253+cgdouthitt@users.noreply.github.com>
2025-02-03 11:04:10 -0600

    Completed course list and by id get route

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const { asyncHandler } = require("./middleware/async-handler");
-const { User } = require("./models");
+const { User, Course } = require("./models");
 const { authenticateUser } = require("./middleware/auth-user");
 
 // Construct a router instance.
@@ -35,7 +35,56 @@ router.post("/users", async (req, res) => {
       error.name === "SequelizeValidationError" ||
       error.name === "SequelizeUniqueConstraintError"
     ) {
-      console.log("catch if");
+      const errors = error.errors.map((err) => err.message);
+      res.status(400).json({ errors });
+    } else {
+      throw error;
+    }
+  }
+});
+
+// Route that returns a list of courses.
+router.get(
+  "/courses",
+  asyncHandler(async (req, res) => {
+    const courses = await Course.findAll({
+      include: [
+        {
+          model: User,
+        },
+      ],
+    });
+    res.json(courses);
+  })
+);
+
+// Route that returns a  course based on the id.
+router.get(
+  "/courses/:id",
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const course = await Course.findByPk(id, {
+      include: [
+        {
+          model: User,
+        },
+      ],
+    });
+    res.json(course);
+  })
+);
+
+// Route that creates a new course.
+router.post("/courses", async (req, res) => {
+  try {
+    await Course.create(req.body);
+    res.set("Location", "/courses/");
+    res.status(201).json({ message: "Course successfully created!" });
+  } catch (error) {
+    if (
+      error.name === "SequelizeValidationError" ||
+      error.name === "SequelizeUniqueConstraintError"
+    ) {
       const errors = error.errors.map((err) => err.message);
       res.status(400).json({ errors });
     } else {
