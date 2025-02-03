3a720e7f62821df82a84fde30629c33f6a570e55
cgdouthitt <46352253+cgdouthitt@users.noreply.github.com>
2025-02-03 11:45:42 -0600

    Completed validation and password hashing work

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 "use strict";
 const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
 // const { Sequelize } = require(".");
 module.exports = (sequelize) => {
   class User extends Model {}
@@ -7,15 +8,61 @@ module.exports = (sequelize) => {
     {
       firstName: {
         type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "A firstName is required",
+          },
+          notEmpty: {
+            msg: "Please provide a firstName",
+          },
+        },
       },
       lastName: {
         type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "A lastName is required",
+          },
+          notEmpty: {
+            msg: "Please provide a lastName",
+          },
+        },
       },
       emailAddress: {
         type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+          msg: "The emailAddress you entered already exists",
+        },
+        validate: {
+          isEmail: {
+            msg: "Please enter a valid emailAddress",
+          },
+          notNull: {
+            msg: "An emailAddress is required",
+          },
+          notEmpty: {
+            msg: "Please provide an emailAddress",
+          },
+        },
       },
       password: {
         type: DataTypes.STRING,
+        allowNull: false,
+        set(val) {
+          const hashedPassword = bcrypt.hashSync(val, 10);
+          this.setDataValue("password", hashedPassword);
+        },
+        validate: {
+          notNull: {
+            msg: "A password is required",
+          },
+          notEmpty: {
+            msg: "Please provide a password",
+          },
+        },
       },
     },
     {
